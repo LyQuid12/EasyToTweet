a204b2d448c1a0e40740778c16e5a88a98b2f5d5
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-16 18:32:39 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-consumer_key=""
-consumer_secret=""
-callback="http://127.0.0.1:5000/callback" # localhost callback for development
+import os
+
+consumer_key=os.environ.get("consumer_key")
+consumer_secret=os.environ.get("consumer_secret")
+callback=os.environ.get("callback")
