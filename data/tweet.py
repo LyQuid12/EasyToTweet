5446575ef5452ae0a891c6c873d921aa0c888733
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-19 17:29:12 +0700

    Create tweet.py

---
+++ data/tweet.py
@@ -0,0 +1,45 @@
+import gistyc
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+from config import *
+
+gist = gistyc.GISTyc(auth_token=gist_token)
+
+def check_update(filepath:str=os.getcwd()+'/data/tweet.json'):
+    gist = 'https://gist.github.com/LyQuid12/'+gist_id
+    html = requests.get(gist).text
+    soup = BeautifulSoup(html, 'html.parser')
+    raw_button = soup.find_all('div', class_='file-actions flex-order-2 pt-0')
+    raw_url = f"https://gist.github.com/{raw_button[0].contents[1].attrs['href'][1:]}"
+    raw_data = requests.get(raw_url).json()["count"]
+    
+    with open(filepath, 'r') as tw:
+        data = json.load(tw)["count"]
+
+    if data <= raw_data-1:
+        tw.close()
+        with open(filepath, 'r+') as count:
+            count_data = json.load(count)
+            count_data["count"] = raw_data
+            count.seek(0)
+            json.dump(count_data, count)
+            count.truncate()
+            count.close()
+            return True
+    else:
+        tw.close()
+        return False
+
+def update_gist(filepath:str=os.getcwd()+'/data/tweet.json'):
+    gist.update_gist(file_name=filepath, gist_id=gist_id)
+
+def update_count(filepath:str=os.getcwd()+'/data/tweet.json'):
+	with open(filepath, 'r+') as j:
+		data = json.load(j)
+		data["count"] += 1
+		j.seek(0)
+		json.dump(data, j)
+		j.truncate()
+		j.close()
