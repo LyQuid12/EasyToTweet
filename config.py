4e0c15c94165f8bbabf619b46c440f8a4cdee80e
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-19 17:31:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 consumer_key=os.environ.get("consumer_key")
 consumer_secret=os.environ.get("consumer_secret")
 callback=os.environ.get("callback")
+gist_token=os.environ.get("gist_token")
+gist_id=os.environ.get("gist_id")
 port=os.environ.get("port", 8000)
 isdebug=False
